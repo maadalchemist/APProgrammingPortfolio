bbe4efa2f93039171ff7a0cd0b506fda49bf8803
maad_alchemist <josephwardle02@gmail.com>
2020-09-02 17:45:33 -0600

    yea

--- Semester 1/Big Java Chapter 1/Calculate Pi/java/Main.java
+++ Semester 1/Big Java Chapter 1/Calculate Pi/java/Main.java
@@ -1,10 +1,20 @@
 public class Main {
   public static void main(String[] args) {
     double pi = 1;
-    for (int i = 3; i < 10000000; i += 2) {
-      pi -= 1/i;
+    double num1 = 1;
+    boolean subtract = true;
+    System.out.println("Loading...");
+    for (int i = 3; i < 2147483647; i += 2) {
+      num1 = i;
+      num1 = 1/num1;
+      if (subtract) {
+        pi += -num1;
+      }else {
+        pi += num1;
+      }
+      subtract = !subtract;
     }
-    pi *= 4;
+    pi = pi*4;
     System.out.println(pi);
   }
 }
