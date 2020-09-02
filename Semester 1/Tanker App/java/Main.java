818e75dbcce6a71ab58b28c11719f86c58f57586
maad_alchemist <josephwardle02@gmail.com>
2020-09-02 11:03:00 -0600

    Added smarter input logic

--- Semester 1/Tanker App/java/Main.java
+++ Semester 1/Tanker App/java/Main.java
@@ -2,15 +2,37 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    double oz_per_day, lifespan_yrs, tankers_consumed;
+    double oz_per_day = 0, lifespan_yrs = 0, tankers_consumed;
     Scanner sin = new Scanner(System.in);
+    boolean correct_input = false;
 
     System.out.println("Welcome to the taker app!");
-    System.out.println("Please input the rate you will consume fluid in ounces per day");
-    oz_per_day = sin.nextDouble();
+    while (correct_input == false){
+      System.out.println("Please input the rate you will consume fluid in ounces per day");
+      oz_per_day = sin.nextDouble();
+      if (oz_per_day < 0){
+        System.out.println("Please input a number greater than 0!");
+      }else if (oz_per_day > 1000){
+        System.out.println("That number is way too large!");
+      }else {
+        correct_input = true;
+      }
+    }
+    correct_input = false;
 
-    System.out.println("Please input your expected lifespan in years");
-    lifespan_yrs = sin.nextDouble();
+    while (correct_input == false){
+      System.out.println("Please input your expected lifespan in years");
+      lifespan_yrs = sin.nextDouble();
+      if (lifespan_yrs < 0){
+        System.out.println("Please input a number greater than 0!");
+      }else if (lifespan_yrs > 1000){
+        System.out.println("That number is way too large!");
+      }else {
+        correct_input = true;
+      }
+    }
+    correct_input = false;
+
 
     tankers_consumed = (oz_per_day * 365 * lifespan_yrs)/128 / 11000;
     System.out.println("Based of of that, you will consume " + tankers_consumed + " 11,000 gallon tankers in your lifetime!");
