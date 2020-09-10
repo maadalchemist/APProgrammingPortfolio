bd83d72f54f7f851a263b58e1f1fd5cd0d55f982
maad_alchemist <31416356+maadalchemist@users.noreply.github.com>
2020-09-10 08:06:10 -0600

    Update Main.java

--- Semester 1/Big Java Chapter 1/Lawn Mower/java/Main.java
+++ Semester 1/Big Java Chapter 1/Lawn Mower/java/Main.java
@@ -7,9 +7,19 @@ public class Main {
       Scanner cin = new Scanner(System.in);
 
       // Welcome the user
+      System.out.println("Welcome to the lawm mower app");
+      
       // Ask user for lawn width
+      System.out.println("Please input the width of the lawn you want to construct");
+      lawnW = cin.nextDouble();
+      
       // Ask user for lawn height
+      System.out.println("Please input the height of the lawn");
+      lawnH = cin.nextDouble();
+      
       // Generate lawn array
+      lawn = new char[lawnW][lawnH];
+      
       // Construct LawnMower object
       // while lawn not mowed
         // do mower stuff
