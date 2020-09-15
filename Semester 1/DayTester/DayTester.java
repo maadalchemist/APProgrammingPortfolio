b5113eb3b32acdd159d1164a61289c62f49183a8
maad_alchemist <josephwardle02@gmail.com>
2020-09-15 11:39:31 -0600

    cleaned/increased effinciency of DayTester

--- Semester 1/DayTester/DayTester.java
+++ Semester 1/DayTester/DayTester.java
@@ -4,19 +4,11 @@ public class DayTester {
   public static void main(String[] args) {
       Day today = new Day();
       Day userBDay;
-
       Scanner cin = new Scanner(System.in);
-      int bDayY, bDayM, bDayD;
 
-      System.out.println("\nWelcome to this console application where we ask \"How many days have you been alive?\"");
-      System.out.println("What is your birthday? (mm dd yyyy)");
-      bDayM = cin.nextInt();
-      bDayD = cin.nextInt();
-      bDayY = cin.nextInt();
-
-      userBDay = new Day(bDayY, bDayM, bDayD);
-
+      System.out.println("\nWelcome to this console application where we ask: \"How many days have you been alive?\"");
+      System.out.println("What is your birthday? (yyyy mm dd)");
+      userBDay = new Day(cin.nextInt(), cin.nextInt(), cin.nextInt());
       System.out.println("You have been alive for " + today.daysFrom(userBDay) + " days!\n");
-
   }
 }
