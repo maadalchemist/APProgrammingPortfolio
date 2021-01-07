64505d509ae33f87cd74d017c2b8abbff8cb01ad
maad_alchemist <josephwardle02@gmail.com>
2021-01-06 20:37:13 -0700

    stuff

--- Semester 1/Big Java/Chapter 5/Main.java
+++ Semester 1/Big Java/Chapter 5/Main.java
@@ -1,37 +1,61 @@
 import java.util.Scanner;
 
 public class Main {
+    enum Season {
+        WINTER,
+        SPRING,
+        SUMMER,
+        FALL {
+            @Override
+            public Season next() {
+                return WINTER;
+            };
+        };
+
+        public Season next() {
+            return values()[ordinal() + 1];
+        }
+    }
+
     public static void main(String[] args) {
-        // If month is 1, 2, or 3, season = "Winter"
-        // Else if month is 4, 5, or 6, season = "Spring"
-        // Else if month is 7, 8, or 9, season = "Summer"
-        // Else if month is 10, 11, or 12, season = "Fall"
-        // If month is divisible by 3 and day >= 21
-        //   If season is "Winter", season = "Spring"
-        //   Else if season is "Spring", season = "Summer"
-        //   Else if season is "Summer", season = "Fall"
-        //   Else season = "Winter"
         int month = 0;
         int day = 0;
+        Season season = Season.SPRING;
         Scanner in = new Scanner(System.in);
         System.out.print("Month: ");
+
         while (month == 0) {
             if (in.hasNextInt()) {
-                if (in.nextInt() > 0 && in.nextInt() <= 12) {
-                    month = in.nextInt();
+                int next_int = in.nextInt();
+                if (next_int > 0 && next_int <= 12) {
+                    month = next_int;
                     break;
                 }
             }
-            System.out.println("\nPlease input a valid month");
+            System.out.print("Please input a valid month: ");
         }
+
+        System.out.print("Day: ");
         while (day == 0) {
             if (in.hasNextInt()) {
-                if (in.nextInt() > 0 && in.nextInt() <= 31) {
-                    day = in.nextInt();
+                int next_int = in.nextInt();
+                if (next_int > 0 && next_int <= 31) {
+                    day = next_int;
                     break;
                 }
             }
-            System.out.println("\nPlease input a valid day");
+            System.out.print("Please input a valid day: ");
         }
+
+        if (month <=1 && month <= 3) {  season = Season.WINTER;  }
+        else if (month <=4 && month <= 6) {  season = Season.SPRING;  }
+        else if (month <=7 && month <= 9) {  season = Season.SUMMER;  }
+        else if (month <=11 && month <= 12) {  season = Season.FALL;  }
+
+        if (month % 3 == 0) {
+            season = season.next();
+        }
+
+        System.out.print("Season: " + season);
     }
 }
