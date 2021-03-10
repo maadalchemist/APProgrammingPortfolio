6763482dfc879b712a1192e1a534a0db61a0e42f
maad_alchemist <josephwardle02@gmail.com>
2021-03-10 00:32:10 -0700

    Recursion study

--- Semester 2/Recursion Testing/Math.java
+++ Semester 2/Recursion Testing/Math.java
@@ -1,8 +1,6 @@
 import java.math.BigInteger;
 
 public class Math {
-    // public static final BigInteger ZERO;
-    // public static final BigInteger ONE;
 
     public Math() {}
 
@@ -12,23 +10,62 @@ public class Math {
         else { return n.multiply(r_factorial(n.subtract(BigInteger.ONE))); }
     }
     BigInteger r_factorial(int n) {
-        Integer integer = Integer.valueOf(n);
-        return r_factorial(BigInteger.valueOf(integer.intValue()));
+        // Integer integer = Integer.valueOf(n);
+        return r_factorial(BigInteger.valueOf(Integer.valueOf(n).intValue()));
     }
 
     /* interative factorial function */
-    int i_factorial (int n) {
-        int out = n;
-        for (int i = n - 1; i > 0; i++) {
-            out *= n;
-            n --;
+    BigInteger i_factorial (BigInteger n) {
+        BigInteger out = BigInteger.ONE;
+        for (BigInteger i = BigInteger.ONE; i.compareTo(n) <= 0; i = i.add(BigInteger.ONE)) {
+            out = out.multiply(i);
         }
         return out;
     }
+    BigInteger i_factorial(int n) {
+        Integer integer = Integer.valueOf(n);
+        return i_factorial(BigInteger.valueOf(integer.intValue()));
+    }
 
     /* recursive fibbinacci function */
-    //int r_fibbinacci(int n) {}
+    BigInteger r_fibbinacci (BigInteger n) {
+        if (n.equals(BigInteger.ZERO)) { return BigInteger.ZERO; }
+        else if (n.equals(BigInteger.ONE)) { return BigInteger.ONE; }
+        return r_fibbinacci(n.subtract(BigInteger.ONE)).add(r_fibbinacci(n.subtract(BigInteger.ONE.add(BigInteger.ONE))));
+    }
+    BigInteger r_fibbinacci(int n) {
+        // Integer integer = Integer.valueOf(n);
+        return r_fibbinacci(BigInteger.valueOf(Integer.valueOf(n).intValue()));
+    }
 
     /* iterative fibbinacci function */
-    //int i_fibbinacci(int n) {}
+    BigInteger i_fibbinacci (BigInteger n) {
+        if (n.equals(BigInteger.ONE)) { return BigInteger.ONE; }
+        else if (n.equals(BigInteger.ONE.add(BigInteger.ONE))) { return BigInteger.ONE; }
+        BigInteger prevPrev = BigInteger.ZERO;
+        BigInteger prev = BigInteger.ONE;
+        BigInteger current = BigInteger.ONE;
+        for (BigInteger i = BigInteger.ONE; i.compareTo(n) < 0; i = i.add(BigInteger.ONE)) {
+            current = prevPrev.add(prev);
+            prevPrev = prev;
+            prev = current;
+        }
+        return current;
+    }
+    BigInteger i_fibbinacci(int n) {
+        Integer integer = Integer.valueOf(n);
+        return i_fibbinacci(BigInteger.valueOf(integer.intValue()));
+    }
 }
+
+/*
+Hardware: AMD Ryzen 5 3600, 16 GB RAM
+
+Factorial functions with 8192 interations:
+Factorial recursive execution time (in nanos): 70797434600
+Factorial iterative execution time (in nanos): 69161929800
+
+Fibbinacci functions with 50 interations:
+Fibbinacci recursive execution time (in nanos): 10959284100
+Fibbinacci recursive execution time (in nanos): 4285600
+*/
--- Semester 2/Recursion Testing/Tester.java
+++ Semester 2/Recursion Testing/Tester.java
@@ -8,17 +8,49 @@ public class Tester {
 
         System.out.println("#####################################################################################################################################\n" +
                            "#                                                                                                                                   #\n" +
-                           "#                                                   Fibbinacci Testing (Recursive)                                                  #\n" +
+                           "#                                                    Factorial Testing (Recursive)                                                  #\n" +
                            "#                                                                                                                                   #\n" +
                            "#####################################################################################################################################\n");
         long startTime = System.nanoTime();
-        for(int i = 2; i < 256; i++) {
+        for(int i = 1; i < 8192; i++) {
             long sTime = System.nanoTime();
             System.out.println(i + ": " + math.r_factorial(i));
-            System.out.println("time elapsed (in millis): " + (System.nanoTime() - sTime));
-            System.out.println("\n");
+            System.out.println("Time elapsed (in nanos): " + (System.nanoTime() - sTime));
+            System.out.println();
         }
         long endTime = System.nanoTime();
+        long fac_rec = endTime - startTime;
+
+
+        System.out.println("#####################################################################################################################################\n" +
+                           "#                                                                                                                                   #\n" +
+                           "#                                                    Factorial Testing (Iterative)                                                  #\n" +
+                           "#                                                                                                                                   #\n" +
+                           "#####################################################################################################################################\n");
+        startTime = System.nanoTime();
+        for(int i = 0; i < 8192; i++) {
+            long sTime = System.nanoTime();
+            System.out.println(i + ": " + math.i_factorial(i));
+            System.out.println("Time elapsed (in nanos): " + (System.nanoTime() - sTime));
+            System.out.println();
+        }
+        endTime = System.nanoTime();
+        long fac_ite = endTime - startTime;
+
+
+        System.out.println("#####################################################################################################################################\n" +
+                           "#                                                                                                                                   #\n" +
+                           "#                                                   Fibbinacci Testing (Recursive)                                                  #\n" +
+                           "#                                                                                                                                   #\n" +
+                           "#####################################################################################################################################\n");
+        startTime = System.nanoTime();
+        for(int i = 1; i < 40; i++) {
+            long sTime = System.nanoTime();
+            System.out.println(i + ": " + math.r_fibbinacci(i));
+            System.out.println("Time elapsed (in nanos): " + (System.nanoTime() - sTime));
+            System.out.println();
+        }
+        endTime = System.nanoTime();
         long fib_rec = endTime - startTime;
 
 
@@ -28,13 +60,27 @@ public class Tester {
                            "#                                                                                                                                   #\n" +
                            "#####################################################################################################################################\n");
         startTime = System.nanoTime();
-        for(int i = 2; i < 256; i++) {
+        for(int i = 1; i < 40; i++) {
             long sTime = System.nanoTime();
-            System.out.println(i + ": " + math.r_factorial(i));
-            System.out.println("time elapsed (in millis): " + (System.nanoTime() - sTime));
-            System.out.println("\n");
+            System.out.println(i + ": " + math.i_fibbinacci(i));
+            System.out.println("Time elapsed (in nanos): " + (System.nanoTime() - sTime));
+            System.out.println();
         }
         endTime = System.nanoTime();
         long fib_ite = endTime - startTime;
+
+
+        System.out.println("#####################################################################################################################################\n" +
+                           "#                                                                                                                                   #\n" +
+                           "#                                                                RESULTS                                                            #\n" +
+                           "#                                                                                                                                   #\n" +
+                           "#####################################################################################################################################\n\n");
+        System.out.println("Factorial functions with 2048 interations:");
+        System.out.println("Factorial recursive execution time (in nanos): " + fac_rec);
+        System.out.println("Factorial iterative execution time (in nanos): " + fac_ite);
+        System.out.println();
+        System.out.println("Fibbinacci functions with 50 interations:");
+        System.out.println("Fibbinacci recursive execution time (in nanos): " + fib_rec);
+        System.out.println("Fibbinacci recursive execution time (in nanos): " + fib_ite);
     }
 }
